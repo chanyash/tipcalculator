c1bc7654e573db49033ceeeea3c3da02549b0a5e
Joanna Chan <chanyash@yahoo-inc.com>
2015-01-20 03:57:30 -0800

    add files

---
+++ tipcalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  tipcalculator
+//
+//  Created by Joanna Chan on 1/20/15.
+//  Copyright (c) 2015 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
