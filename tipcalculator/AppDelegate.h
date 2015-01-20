ca3b6e77c3f75dab5d3085e55641c6fdd84d0550
Joanna Chan <chanyash@yahoo-inc.com>
2015-01-19 23:32:15 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Joanna Chan on 1/19/15.
+//  Copyright (c) 2015 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
